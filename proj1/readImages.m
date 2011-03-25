747190dedf632d533d462b2e52a3734e8e9ee673
drakeguan <drake.guan@gmail.com>
2011-03-25 09:46:55 +0800

    Change the arguments of readImages to fit the natural matrix property in matlab.
    One of matlab's strong matrix indexing is to use 'linear indexing', which is
    column oriented cause it traverses the whole matrix by fixing the latter
    subscripts or dimensions. That is, it encourages us to put dimension of
    global view or bigger concept in latter.

--- proj1/readImages.m
+++ proj1/readImages.m
@@ -1,4 +1,4 @@
-function [imgs, exposureTimes] = readImages(folder, extension)
+function [images, exposureTimes] = readImages(folder, extension)
 % read in several images with different exposures.
 %
 % input
@@ -6,14 +6,15 @@ function [imgs, exposureTimes] = readImages(folder, extension)
 %  extension: file extension. default to 'jpg'.
 %
 % output
-%  imgs: 4 dimensional matrices, representing iamge set
-%	[i, row, col, channel]
-%  exposureTimes: (n, 1) matrices, representing image's exposure time in second.
+%  images: 4 dimensional matrices, representing the whole image set.
+%	[row, col, channel, i] for i = 1:number of images.
+%  exposureTimes: (number, 1) matrices, representing image's exposure time in second.
 %
 % note
-%  we assume the input images have the same dimension, channel and color space.
+%  We assume the input images have the same dimension, channel number and color space.
+%  
 %
-    imgs = [];
+    images = [];
     exposureTimes = [];
 
     if( ~exist('extension') )
@@ -21,16 +22,16 @@ function [imgs, exposureTimes] = readImages(folder, extension)
     end
 
     files = dir([folder, '/*.', extension]);
-    n = length(files);
-    exposureTimes = zeros(n, 1);
-    for i = 1:n
+    number = length(files);
+    exposureTimes = zeros(number, 1);
+    for i = 1:number
 	filename = [folder, '/', files(i).name];
 	img = double(imread(filename));
 	[row, col, channel] = size(img);
 	if( i == 1 )
-	    imgs = zeros(n, row, col, channel);
+	    images = zeros(row, col, channel, number);
 	end
-	imgs(i,:,:,:) = img;
+	images(:,:,:,i) = img;
 
 	try
 	    exif = exifread(filename);
